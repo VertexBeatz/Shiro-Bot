333e31c64ddf6aeac6f5095f161ac86468faa701
VertexBeatz <35179869+VertexBeatz@users.noreply.github.com>
2018-04-30 18:57:02 -0400

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install python3.5-dev python3-pip build-essential libssl-dev libffi-dev git ffmpeg libopus-dev unzip -y
+sudo -H pip3 install --upgrade pip
+sudo pip3 install emoji --upgrade
+sudo pip3 install tabulate
+sudo pip3 install beautifulsoup4
